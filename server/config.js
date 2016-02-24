38602b7b902673755ca9c2558a79293a8c4d5162
Ross Davis <Ross.ad@gmail.com>
2016-02-24 13:05:36 -0800

    Building Server routing

--- server/config.js
+++ server/config.js
@@ -10,3 +10,5 @@ db.serialize(function(){
     db.run('CREATE TABLE Events (eventId INTEGER PRIMARY KEY AUTOINCREMENT, eventOwn VARCHAR, title VARCHAR, date DATE, time TIME, streetAddress VARCHAR, city VARCHAR, state VARCHAR, zip INTEGER, lat REAL, lon REAL, inOut INTEGER, estWx VARCHAR, wxSts VARCHAR, pubPriv INTEGER )');
   }
 });
+
+exports.db = db;
