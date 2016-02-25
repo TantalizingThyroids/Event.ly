2c8f9f174bd306edcfad8127e205fadb255d07e8
Linda Zou <lindazou@Lindas-MacBook-Pro.local>
2016-02-24 19:16:43 -0500

    Completed mvp controller, router. Incorporated api - needs integration testing with other components

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,73 +1,12 @@
 var db = require('../db/db.js');
-var helpers = require('./helpers.js'); // our custom middleware
+var helpers = require('./helpers.js'); 
 
 module.exports = function (app, express) {
   app.get('/api/');
   app.get('/api/event', db.getAll);
   app.post('/api/event', db.addOne);
   app.get('api/event/:id', db.getByOwner);
-  app.delete('/api/event', db.deleteEvent);
-
-
-//=========================================
-
-// app.get('/api/', function(req, res) {
-//   if(req.method === "GET") {
-//     function(err, data){
-//       if(err) throw err;
-//         res.writeHead(200);
-//         res.end(data);
-//       };
-//   } 
-// });
-
-// app.get('/api/event', function(req, res) {
-//   if(req.method === "GET"){
-//       db.getAll(req.url, function(exist, data){
-//         if(!exist){
-//           res.writeHead(404);
-//           res.end();
-//         } else {
-//           res.writeHead(200);
-//           res.end(data);
-//         }
-//       });
-//     }
-
-// app.post('/api/', function(req, res) {
-  // var body = '';
-  //   req.on('data', function(data){
-  //     body += data;
-  // });
-//   db.addOne(body, function() {});
-//     res.writeHead(302);
-//     function(err, data){
-//       if(err) throw err;
-//       res.end(data);
-//   };
-// })
-
-// app.post('/api/', function(req, res) {
-  // var body = '';
-  //   req.on('data', function(data){
-  //     body += data;
-  // });
-//   db.deleteOne(body, function() {});
-//     res.writeHead(302);
-//     function(err, data){
-//       if(err) throw err;
-//       res.end(data);
-//   };
-// })
-
-//=========================================
-
-  // authentication middleware used to decode token and made available on the request
-  // app.use('/api/links', helpers.decode);
-
-
-  // If a request is sent somewhere other than the routes above,
-  // send it through our custom error handler
+  app.delete('/api/event:id', db.deleteEvent);
 
   app.use(helpers.errorLogger);
   app.use(helpers.errorHandler);
--- server/server.js
+++ server/server.js
@@ -1,13 +1,11 @@
-// var path = require('path');
-// var fs = require('fs');
 var express = require('express');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
 var sqlite = require('sqlite3');
 var bluebird = require('bluebird');
-// var weatherInfo = require('./wundergroundapi/wundergroundapi');
-// var db = require('../db/db.js');
-// console.log(db);
+var weatherInfo = require('./wundergroundapi/wundergroundapi');
+var db = require('../db/db.js');
+
 
 var app = express();
 
@@ -21,99 +19,79 @@ app.get('/api/',
 	function(req, res) {
 		// console.log(__dirname)
 		// res.writeHead(200);
-		res.sendFile(__dirname + '/testindex.html');
+		res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/api/event', function(req, res) {
+  var newEvent = req.body;
+  console.log("newEvent", newEvent);
+  db.create(newEvent, function (err, data) {
+    if(err) {
+      return res.status(500).json(err);
+    }
+    res.status(201).json(data);
+  });
 });
 
 app.get('/api/event', 
-	function(req, res) {
-		// res.json({ message: 'hooray! welcome to our api!' });   
-		db.getAll(function(data) {
-			res.send(data);
-		})
-});
+  function(req, res) {
+    db.getAll(function (err, data) {
+      if(err) {
+        return res.status(500).json(err);
+      }
+      res.json(data);
+    });
+  });
 
-app.post('/api/event', 
+app.post('/api/weather', 
 	function(req, res) {
-		var wx, location, weather;
+		// var wx, location, weather;
 		app.get('http://api.wunderground.com/api/70ba34089d4744a1/hourly10day/q/' + req.body.zipcode + '.json', 
 		  	function(parsed_json) {
 			  var location = parsed_json['location']['city'];
 			  var wx = parsed_json['current_observation']['temp_f'];
 			  var test = console.log(wx)
-			  	req.body[weather] = wx;
-			  		res.send(test)
+			  	// req.body[weather] = wx;
+			  		res.json(location, wx);
 
 	  		})
 	// db.addOne(req.body, 
 	// 	function() {
-			res.send("test")
+			res.json(location, wx);
 	// })
 });
 
 app.delete('/api/event',
-	function(req, res) {
-	db.deleteEvent(req.body.eventId, function() {
-	res.send("Event has been deleted!");
-	})
+function(req, res) {
+  console.log("req.body is: ", req.body)
+  console.log("req.params.id is: ", req.params.id)
+  // var deletedItem = req.body;
+  var eventId = req.params.id;
+  db.deleteEvent(eventId, function (err, data) {
+    if(err) {
+      return res.status(500).json(err);
+    } else {
+      console.log('eventId is: ', eventId);
+      res.json(data);
+    }
+  });
 });
 
 app.get('/api/event',
-	function(req, res) {
-	db.getByOwner(req.body.eventOwner, function(data) {
-	res.send(data)
-	})
+function(req, res) {
+  console.log("req.body is: ", req.body)
+  console.log("req.params.id is: ", req.params.id)
+  // var deletedItem = req.body;
+  var eventId = req.params.id;
+  db.getByOwner(eventId, function (err, data) {
+    if(err) {
+      return res.status(500).json(err);
+    } else {
+      console.log('eventId is: ', eventId);
+      res.json(data);
+    }
+  });
 });
-
-// app.get('/api/event', 
-// 	function(req, res) {
-//     function (err, data){
-//       if(err) {
-//       	throw err
-//       } else {
-//         res.writeHead(200);
-//       }
-//     };
-//   } 
-// });
-
-// // app.get('/api/event', function(req, res) {
-// //   if(req.method === "GET"){
-// //       db.getAll(req.url, function(exist, data){
-// //         if(!exist){
-// //           res.writeHead(404);
-//           res.end();
-//         } else {
-//           res.writeHead(200);
-//           res.end(data);
-//         }
-//       });
-//     }
-
-// app.post('/api/', function(req, res) {
-  // var body = '';
-  //   req.on('data', function(data){
-  //     body += data;
-  // });
-//   db.addOne(body, function() {});
-//     res.writeHead(302);
-//     function(err, data){
-//       if(err) throw err;
-//       res.end(data);
-//   };
-// })
-
-// app.post('/api/', function(req, res) {
-  // var body = '';
-  //   req.on('data', function(data){
-  //     body += data;
-  // });
-//   db.deleteOne(body, function() {});
-//     res.writeHead(302);
-//     function(err, data){
-//       if(err) throw err;
-//       res.end(data);
-//   };
-// })
 
 app.use(express.static(__dirname + '/../client'));
 
