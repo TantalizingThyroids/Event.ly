8635ad2150ac2911a6c63341897dda92f8493127
Ross Davis <Ross.ad@gmail.com>
2016-02-29 17:37:17 -0800

    add email functionality, emails on status change

--- server/wundergroundapi/wundergroundapi.js
+++ server/wundergroundapi/wundergroundapi.js
@@ -3,6 +3,16 @@ var eventController = require('../controllers/eventController.js');
 var https = require('https');
 // var Promise = require('bluebird');
 var request = require('request');
+var Mailgun = require('mailgun-js');
+
+//Api key, from Mailgun’s Control Panel
+var api_key = 'key-e1b50883b2949b370d9bbf222a188a2b';
+
+//Your domain, from the Mailgun Control Panel
+var domain = 'sandbox2fd7054e03fc4074b11daafae9b2e9eb.mailgun.org';
+
+//Your sending email address
+var from_who = 'ross.ad@gmail.com';
 
 var wxTerm = {
 'Chance of Flurries':-1,
@@ -95,10 +105,33 @@ module.exports.wxCheck = function(userID){
 
           event.updateWx(estWx, id);
           event.updateStamp(timeStamp, id);
-          
+          console.log('Owner Email: ', item.eventOwner);
           if(wxTerm[currForc.conditions] !== item.weatherStatus){
             event.updateWx(wxTerm[currForc.conditions], id);
             event.updateStamp(timeStamp, id);
+            var mailgun = new Mailgun({apiKey: api_key, domain: domain});
+            var mailData = {
+                //Specify email data
+                  from: from_who,
+                //The email to contact
+                  to: item.eventOwner,
+                //Subject and text data  
+                  subject: 'Hello from Event.ly!!',
+                  html: 'Hello! You are recieving this email due to a change in weather conditions for your scheduled event.  Current Event information is as follows:'+ 
+                    '<div><h2>Title:'+item.title+'</h2><p>Event ID:'+item.eventID+'</p><p>Event Owner Email:'+item.eventOwner+'</p><p>Time:'+item.time+'</p><p>Date:'+item.date+'</p><p>Street Address:'+item.streetAddress+'</p><p>City:'+item.city+'</p><p>State:'+item.state+'</p><p>Zip Code:'+item.zipCode+'</p><p>Outside?'+item.indoorOutdoor+'</p><p>Estimated Weather:'+item.estimatedWeather+'</p><p>Current Weather Status:'+item.weatherStatus+'</p><p>Last Update:'+item.lastUpdate+'</p></div>'
+                };
+            mailgun.messages().send(mailData, function (err, body) {
+                    //If there is an error, render the error page
+                    if (err) {
+                        console.log("got an error: ", err);
+                    }
+                    //Else we can greet    and leave
+                    else {
+                        //Here "submitted.jade" is the view file for this landing page 
+                        //We pass the variable "email" from the url parameter in an object rendered by Jade
+                        console.log(body);
+                    }
+                });
             // Email change in weather status to user
           }
         });
